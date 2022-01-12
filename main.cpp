7aeaa214ac59982406beb0c9891c9ce7ba7d6fa0
vmrepo <crash221b@gmail.com>
2022-01-11 23:18:36 -0700

    stuff mang

--- main.cpp
+++ main.cpp
@@ -1,21 +1,32 @@
-#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>                                                
 #include "Config.hpp"
+#include <cmath>
 
 int main()
 {
     Config con;
     con.readText("config.txt");
-    sf::RenderWindow window(sf::VideoMode(200, 200), con.array[0]);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Demo Tool");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+                {
+                    sf::Vector2i p = sf::Mouse::getPosition(window);
+                    sf::Vector2f pp = shape.getPosition();
+                    printf("%d %d\n", p.x, p.y);
+                }
+                if (event.type == sf::Event::Closed)
+                {
+                    window.close();
+                }
+            }
         }
 
         window.clear();
